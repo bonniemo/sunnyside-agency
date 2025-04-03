3786b78fdae175217198281432bce406b644a3ae
bonniemo <moller.bon@gmail.com>
2025-04-03 21:15:29 +0200

    adjusted lp cards styles

--- components/LandingPageCards/StandOutTxt.tsx
+++ components/LandingPageCards/StandOutTxt.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const StandOutTxt = () => {
     return (
-        <section className="py-l md:p-largest px-s text-center">
+        <section className="flex flex-col items-center justify-center px-s text-center h-full">
             <h2 className="text-h2-small font-fraunces font-bold">
                 Stand out to the <span className="block">right audience</span>
             </h2>
-            <p className="mt-xs text-dark-grayish-blue">
+            <p className="mt-xs text-dark-grayish-blue lg:max-w-[80%] 2xl:max-w-[50%]">
                 Using a collaborative formula of designers, researchers,
                 photographers, videographers, and copywriters, we’ll build and
                 extend your brand in digital places.
--- components/LandingPageCards/TransformTxt.tsx
+++ components/LandingPageCards/TransformTxt.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const TransformTxt = () => {
     return (
-        <section className="py-l md:p-largest px-s text-center">
+        <section className="flex flex-col items-center justify-center px-s text-center h-full">
             <h2 className="text-h2-small font-fraunces font-bold">
                 Transform your <span className="block">brand</span>
             </h2>
-            <p className="mt-xs text-dark-grayish-blue">
+            <p className="mt-xs text-dark-grayish-blue lg:max-w-[80%] 2xl:max-w-[50%]">
                 We are a full-service creative agency specializing in helping
                 brands grow fast. Engage your clients through compelling visuals
                 that do most of the marketing for you.
